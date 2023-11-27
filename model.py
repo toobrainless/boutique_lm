feat(train)!: add mixed precision and flash attention fix: fix order of batch and seq axis

model.py
@@ -5,9 +5,9 @@ from typing import Tuple
 
 import torch
 from torch import Tensor, nn
+from torch.distributions import Categorical
 from torch.nn import TransformerEncoder, TransformerEncoderLayer
 from torch.utils.data import dataset
-from torch.distributions import Categorical
 
 device = torch.device("cuda")
 
@@ -21,9 +21,9 @@ class PositionalEncoding(nn.Module):
         div_term = torch.exp(
             torch.arange(0, d_model, 2) * (-math.log(10000.0) / d_model)
         )
-        pe = torch.zeros(max_len, 1, d_model)
-        pe[:, 0, 0::2] = torch.sin(position * div_term)
-        pe[:, 0, 1::2] = torch.cos(position * div_term)
+        pe = torch.zeros(1, max_len, d_model)
+        pe[0, :, 0::2] = torch.sin(position * div_term)
+        pe[0, :, 1::2] = torch.cos(position * div_term)
         self.register_buffer("pe", pe)
 
     def forward(self, x: Tensor) -> Tensor:
@@ -31,7 +31,7 @@ class PositionalEncoding(nn.Module):
         Arguments:
             x: Tensor, shape ``[seq_len, batch_size, embedding_dim]``
         """
-        x = x + self.pe[: x.size(0)]
+        x = x + self.pe[:, : x.size(1)]
         return self.dropout(x)
 
 
@@ -48,7 +48,13 @@ class TransformerModel(nn.Module):
         super().__init__()
         self.model_type = "Transformer"
         self.pos_encoder = PositionalEncoding(d_model, dropout)
-        encoder_layers = TransformerEncoderLayer(d_model, nhead, d_hid, dropout)
+        encoder_layers = TransformerEncoderLayer(
+            d_model,
+            nhead,
+            d_hid,
+            dropout,
+            batch_first=True,
+        )
         self.transformer_encoder = TransformerEncoder(encoder_layers, nlayers)
         self.embedding = nn.Embedding(ntoken, d_model)
         self.d_model = d_model
@@ -71,11 +77,11 @@ class TransformerModel(nn.Module):
     def forward(self, src: Tensor, src_mask: Tensor = None) -> Tensor:
         """
         Arguments:
-            src: Tensor, shape ``[seq_len, batch_size]``
+            src: Tensor, shape ``[batch_size, seq_len]``
             src_mask: Tensor, shape ``[seq_len, seq_len]``
 
         Returns:
-            output Tensor of shape ``[seq_len, batch_size, ntoken]``
+            output Tensor of shape ``[batch_size, seq_len, ntoken]``
         """
         src = self.embedding(src) * math.sqrt(self.d_model)
         src = self.pos_encoder(src)
@@ -84,7 +90,7 @@ class TransformerModel(nn.Module):
             Unmasked positions are filled with float(0.0).
             """
             src_mask = (
-                nn.Transformer.generate_square_subsequent_mask(len(src))
+                nn.Transformer.generate_square_subsequent_mask(src.shape[1])
                 .to(self.device)
                 .isinf()
             )

train.py
@@ -44,16 +44,16 @@ config = {
     "nhead": 16,  # number of heads in ``nn.MultiheadAttention``
     "dropout": 0.1,  # dropout probability
     "sp_model_prefix": "bpe_2000",
-    "max_length": 512,
-    "batch_size": 80,
+    "max_length": 256,
+    "batch_size": 768,
     "lr": 3e-4,
     "weight_decay": 0,
     "epochs": 100,
     "len_epoch": 5000,
     "log_step": 100,
-    "accumulation_steps": 2,
+    "accumulation_steps": 1,
     "project": "boutique_lm",
-    "name": "test_run4",
+    "name": "test_amp",
     "save_period": 1,
 }
 
@@ -103,7 +103,7 @@ if __name__ == "__main__":
         weight_decay=config["weight_decay"],
     )
     scheduler = torch.optim.lr_scheduler.StepLR(optimizer, 1.0, gamma=0.95)
-
+    scaler = torch.cuda.amp.GradScaler()
     loader = iter(loader)
 
     step = 0
@@ -113,20 +113,28 @@ if __name__ == "__main__":
         total_loss = 0
         total_grad = 0
         for batch_idx in tqdm(range(config["len_epoch"]), desc="train"):
-            optimizer.zero_grad()
+            optimizer.zero_grad(set_to_none=True)
             for accum_step in range(config["accumulation_steps"]):
                 batch = next(loader)
                 src, tgt = batch["src"].to(device), batch["tgt"].to(device)
-                output = model(src)
-                output_flat = output.view(-1, config["vocab_size"])
-                loss = criterion(output_flat, tgt.view(-1))
+                with torch.autocast(device_type="cuda", dtype=torch.float16):
+                    with torch.backends.cuda.sdp_kernel(
+                        enable_flash=True,
+                        enable_math=False,
+                        enable_mem_efficient=False,
+                    ):
+                        output = model(src)
+                        output_flat = output.view(-1, config["vocab_size"])
+                        loss = criterion(output_flat, tgt.view(-1))
 
-                loss.backward()
+                scaler.scale(loss).backward()
                 total_loss += loss.item()
-
+            scaler.unscale_(optimizer)
             torch.nn.utils.clip_grad_norm_(model.parameters(), 0.5)
             total_grad += get_grad_norm(model)
-            optimizer.step()
+            # optimizer.step()
+            scaler.step(optimizer)
+            scaler.update()
 
             if (batch_idx + 1) % config["log_step"] == 0:
                 step = epoch * config["len_epoch"] + batch_idx